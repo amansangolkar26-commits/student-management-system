e9465a72162f2e8e59749485db8e33c8d7454d7e
amansangolkar26-commits <amansangolkar26@gmail.com>
2026-02-21 10:42:33 +0530

    Create Student.java entity model

---
+++ backend/src/main/java/com/studentmgmt/Student.java
@@ -0,0 +1,46 @@
+package com.studentmgmt;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public Student() {
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
